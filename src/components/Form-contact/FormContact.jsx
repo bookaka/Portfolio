09a801ba63975e9227f19c27876bd37ccac5e969
Tran <trandinh02w@gmail.com>
2024-06-04 06:39:48 +0700

    update file and delete console.log

---
+++ src/components/Form-contact/FormContact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./FormContact.scss";
+import Button from "../../components/Button/Button";
+
+function FormContact() {
+  return (
+    <>
+      <form className="contact-form" id="contact-form">
+        <div className="contact-form__info">
+          <div className="contact-form__info-name">
+            <input type="text" placeholder="YOUR NAME" required />
+          </div>
+          <div className="contact-form__info-email">
+            <input type="email" placeholder="YOUR EMAIL" required />
+          </div>
+        </div>
+        <div className="contact-form__subject">
+          <input type="text" placeholder="YOUR SUBJECT" required />
+        </div>
+        <div className="contact-form__message">
+          <input type="text" placeholder="YOUR MESSAGE" required />
+        </div>
+        <Button type="submit">
+          <span className="btn-text">send message</span>
+          <span className="btn-icon">
+            <i className="bx bx-mail-send bx-tada"></i>
+          </span>
+        </Button>
+      </form>
+    </>
+  );
+}
+
+export default FormContact;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./home.scss";
+import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button.jsx";
+
+function home() {
+  window.scrollTo(0, 0);
+  return (
+    <>
+      <div className="slide"></div>
+      <div className="section home" data-aos="fade-up">
+        <div className="color-block"></div>
+        <div className="home__image">
+          <div className="home__image-wrap"></div>
+        </div>
+        <div className="home__text">
+          <div className="home__text-wrap">
+            <h1>Hello,</h1>
+            <h1>My name Tran Quoc Dinh</h1>
+            <h2>I am Web Developer</h2>
+            <p>
+              Hello everyone,My name is Tran Quoc Dinh, and I am currently a
+              fourth-year student at University of Natural Sciences, Ho Chi Minh
+              City, majoring in Information Technology. I have been passionate
+              about computer science since high school, and my goal is to become
+              a web developer.
+            </p>
+            <p>Thank you for visiting my profile!</p>
+            <div className="home__button">
+              <Button className="home__button-btn">
+                <Link to="/portfolio">
+                  <span className="btn-text">Portfolio</span>
+                  <span className="btn-icon">
+                    <i className="bx bx-right-arrow-alt"></i>
+                  </span>
+                </Link>
+              </Button>
+              <Button className="home__button-btn">
+                <Link to="/about">
+                  <span className="btn-text">About</span>
+                  <span className="btn-icon">
+                    <i className="bx bx-right-arrow-alt"></i>
+                  </span>
+                </Link>
+              </Button>
+              <Button className="home__button-btn">
+                <Link to="/contact">
+                  <span className="btn-text">Contact</span>
+                  <span className="btn-icon">
+                    <i className="bx bx-right-arrow-alt"></i>
+                  </span>
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default home;
